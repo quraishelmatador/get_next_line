7878093857195e9e4ae23ba66f020910e96eed2a
quraishelmatador <141680137+quraishelmatador@users.noreply.github.com>
2024-12-07 22:40:48 +0000

    First Commit

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t  ft_strlen(const char *s);
+void    *ft_calloc(size_t nmemb, size_t size);
+void    *ft_calloc(size_t nmemb, size_t size);
+char    *ft_strchr(const char *s, int c);
+char *get_next_line(int fd);
+char    *ft_strjoin(char const *s1, char const *s2);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,80 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*memory;
+
+	if (nmemb && size && nmemb > (SIZE_MAX / size))
+		return (NULL);
+	memory = malloc(nmemb * size);
+	if (!memory)
+		return (NULL);
+	ft_bzero(memory, nmemb * size);
+	return (memory);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	else
+		return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		sizetotal;
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	sizetotal = ft_strlen(s1) + ft_strlen(s2);
+	res = malloc(sizeof(char) * (sizetotal + 1));
+	if (!res || !s1 || !s2)
+		return (NULL);
+	while (s1[i] != 0)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != 0)
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[sizetotal] = 0;
+	return (res);
+}
