02aaf1b41a3a6ef2f592519eb7e11a62ba9b4367
Kouresh Tayabaly <ktayabal@c1r4s2.42london.com>
2024-12-15 20:16:04 +0000

    commit

---
+++ get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktayabal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 15:25:31 by ktayabal          #+#    #+#             */
+/*   Updated: 2024/12/15 15:25:32 by ktayabal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*buffer;
+	int	reading;
+
+	buffer = (char*)malloc(BUFFER_SIZE + 1);
+	reading = read(fd, buffer, BUFFER_SIZE);
+}
+*/
+	
--- get_next_line.h
+++ get_next_line.h
@@ -3,12 +3,18 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdint.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
 size_t  ft_strlen(const char *s);
 void    *ft_calloc(size_t nmemb, size_t size);
+void	ft_bzero(void *s, size_t n);
 char    *ft_strchr(const char *s, int c);
-char *get_next_line(int fd);
+//char *get_next_line(int fd);
 char    *ft_strjoin(char const *s1, char const *s2);
-void	ft_bzero(void *s, size_t n)
+char	*get_next_line(int fd);
 
 #endif
