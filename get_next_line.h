d3d963f9570fe49dcde91271e228a6ece086fa9b
Kouresh Tayabaly <ktayabal@Kouresh-MacBook-Pro.local>
2024-12-19 16:32:15 +0000

    mac to git

--- get_next_line.h
+++ get_next_line.h
@@ -1,20 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktayabal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 10:20:32 by ktayabal          #+#    #+#             */
+/*   Updated: 2024/12/18 10:20:34 by ktayabal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdint.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 5
 # endif
 
-size_t  ft_strlen(const char *s);
-void    *ft_calloc(size_t nmemb, size_t size);
-void	ft_bzero(void *s, size_t n);
-char    *ft_strchr(const char *s, int c);
-//char *get_next_line(int fd);
-char    *ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*extract_line(char *stash);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktayabal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 21:54:08 by ktayabal          #+#    #+#             */
+/*   Updated: 2024/12/17 21:54:11 by ktayabal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 size_t	ft_strlen(const char *s)
@@ -5,76 +17,75 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (s && s[i])
 		i++;
 	return (i);
 }
 
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*memory;
-
-	if (nmemb && size && nmemb > (SIZE_MAX / size))
-		return (NULL);
-	memory = malloc(nmemb * size);
-	if (!memory)
-		return (NULL);
-	ft_bzero(memory, nmemb * size);
-	return (memory);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	char	*str;
-	size_t	i;
-
-	str = (char *)s;
-	i = 0;
-	while (i < n)
-	{
-		str[i] = '\0';
-		i++;
-	}
-}
-
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
 		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
-	if (*s == (char)c)
+	if (c == '\0')
 		return ((char *)s);
-	else
-		return (NULL);
+	return (NULL);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	int		sizetotal;
-	char	*res;
-	int		i;
-	int		j;
+	char	*result;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
-	sizetotal = ft_strlen(s1) + ft_strlen(s2);
-	res = malloc(sizeof(char) * (sizetotal + 1));
-	if (!res || !s1 || !s2)
-		return (NULL);
-	while (s1[i] != 0)
-	{
-		res[i] = s1[i];
-		i++;
-	}
 	j = 0;
-	while (s2[j] != 0)
+	if (!s2)
+		return (NULL);
+	result = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!result)
 	{
-		res[i] = s2[j];
-		i++;
-		j++;
+		free(s1);
+		return (NULL);
 	}
-	res[sizetotal] = 0;
-	return (res);
+	while (s1 && s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		result[i++] = s2[j++];
+	result[i] = '\0';
+	free(s1);
+	return (result);
+}
+
+char	*extract_line(char *stash)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!stash || !*stash)
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = malloc(i + 2);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
 }
